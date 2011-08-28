71b3647b95e1bdad78ebbaf0285258a7d15a2da1
Laurens de Koning <laurens@laurensdekoning.nl>
2011-08-28 14:13:55 +0200

    Prepared the solution to be multilingual. Added Dutch dictionary resource first.
    Language I intent to support are Dutch, English, German and French.

--- contactsToFritz/MainWindow.xaml.cs
+++ contactsToFritz/MainWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace contactsToFritz
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
